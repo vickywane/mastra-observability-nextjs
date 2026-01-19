f032d6685e6f8e13c1d0926f24d3a6aefad17150
Nwani Victory <vickywane@gmail.com>
2026-01-19 08:00:43 +0000

    update: remove LibSQLStore memory usages for vercel deployment

--- mastra/agents/weather-agent.ts
+++ mastra/agents/weather-agent.ts
@@ -1,6 +1,6 @@
 import { Agent } from '@mastra/core/agent';
-import { Memory } from '@mastra/memory';
-import { LibSQLStore } from '@mastra/libsql';
+// import { Memory } from '@mastra/memory';
+// import { LibSQLStore } from '@mastra/libsql';
 import { weatherTool } from '../tools/weather-tool';
 
 export const weatherAgent = new Agent({
@@ -22,9 +22,9 @@ export const weatherAgent = new Agent({
   model: 'openai/gpt-4o-mini',
   tools: { weatherTool },
 
-  memory: new Memory({
-    storage: new LibSQLStore({
-      url: 'file:../mastra.db', // path is relative to the .mastra/output directory
-    }),
-  }),
+  // memory: new Memory({
+  //   storage: new LibSQLStore({
+  //     url: 'file:../mastra.db', // path is relative to the .mastra/output directory
+  //   }),
+  // }),
 });
